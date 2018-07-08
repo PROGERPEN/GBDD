abecde133016b8cbe38a9c5336efd5c2ea423c0c
progerpen@gmail.com <pbufvtn212>
2018-07-08 17:08:06 +0300

    getting

--- app/src/main/java/recepty/paragraph/com/gbdd/FirstrunActivity.java
+++ app/src/main/java/recepty/paragraph/com/gbdd/FirstrunActivity.java
@@ -1,13 +1,33 @@
 package recepty.paragraph.com.gbdd;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class FirstrunActivity extends AppCompatActivity {
+
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_firstrun);
+
+        final Context context = this;
+
+        button1 = (Button) findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, RegnumberActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
